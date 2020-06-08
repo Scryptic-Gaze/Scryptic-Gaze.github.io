1c894ffda0eeb7dc09081d5ce240e6b921f59fa4
Scryptic-Gaze <65416621+Scryptic-Gaze@users.noreply.github.com>
2020-06-08 19:14:46 +0200

    WorldCup'18_project
    With Quiz

---
+++ script.js
@@ -0,0 +1,54 @@
+function popupMe() {
+    var popup = document.getElementById("my_popup");
+    popup.classList.toggle("show");
+}
+
+function popupMe2() {
+    var popup = document.getElementById("my_popup2");
+    popup.classList.toggle("show");
+}
+
+function popupMe3() {
+    var popup = document.getElementById("my_popup3");
+    popup.classList.toggle("show");
+}
+
+function popupMe4() {
+    var popup = document.getElementById("my_popup4");
+    popup.classList.toggle("show");
+}
+
+function popupMe5() {
+    var popup = document.getElementById("my_popup5");
+    popup.classList.toggle("show");
+}
+
+function popupMe6() {
+    var popup = document.getElementById("my_popup6");
+    popup.classList.toggle("show");
+}
+
+function popupMe7() {
+    var popup = document.getElementById("my_popup7");
+    popup.classList.toggle("show");
+}
+
+function popupMe8() {
+    var popup = document.getElementById("my_popup8");
+    popup.classList.toggle("show");
+}
+
+function popupMe9() {
+    var popup = document.getElementById("my_popup9");
+    popup.classList.toggle("show");
+}
+
+function popupMe10() {
+    var popup = document.getElementById("my_popup10");
+    popup.classList.toggle("show");
+}
+
+function popupMe11() {
+    var popup = document.getElementById("my_popup11");
+    popup.classList.toggle("show");
+}
